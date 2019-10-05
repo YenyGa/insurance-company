de16910b1e2a4738f94bcc250c2534c877247184
YenyGa <garcia.yeny@hotmail.com>
2019-10-05 13:47:40 -0500

    Initial version of insurance generated by JHipster-6.3.1

---
+++ src/main/java/com/insurance/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.insurance.config.audit;
---
+++ src/main/java/com/insurance/service/InvalidPasswordException.java
@@ -0,0 +1,9 @@
+package com.insurance.service;
+
+public class InvalidPasswordException extends RuntimeException {
+
+    public InvalidPasswordException() {
+        super("Incorrect password");
+    }
+
+}
